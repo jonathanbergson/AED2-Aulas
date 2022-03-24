5671a79c296c668d3bd4cd9ddb4d11933089c5cf
Jonathan Bergson <contato@bergson.me>
2022-03-23 21:01:27 -0300

    chore: nova estrutura de dados

---
+++ AED2_Fila/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AED2_Fila
+{
+    class Node
+    {
+        string person;
+        Node next;
+
+        public Node(string name)
+        {
+            person = name;
+            next = null;
+        }
+    }
+}
---
+++ AED2_Fila/Queue.cs
@@ -0,0 +1,25 @@
+﻿namespace AED2_Fila
+{
+    class Queue
+    {
+        public void Toqueue()
+        {
+            // Add
+        }
+
+        public void Dequeue()
+        {
+            // Remove Init
+        }
+
+        public void Count()
+        {
+            
+        }
+
+        public void Lock(int count = 1)
+        {
+            // Show itens
+        }
+    }
+}
